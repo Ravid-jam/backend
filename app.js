b09dcd5ca708f33db7e9bce7121e1c68ef4dd954
Ravid-jam <ravid.jam02@gmail.com>
2024-09-18 15:06:55 +0530

    Bugs

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require("express");
-var path = require("path");
-
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
@@ -25,17 +23,6 @@ app.use(function (req, res, next) {
     message: "Page not found",
     status: 404,
   });
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
 });
 
 module.exports = app;
