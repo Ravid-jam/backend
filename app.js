b884dadd33f5a251e47d47b5bd5a605c80c50c32
Ravid-jam <ravid.jam02@gmail.com>
2024-09-18 14:32:09 +0530

    vercel.json file added.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require("http-errors");
 var express = require("express");
-const dotenv = require("dotenv");
 var path = require("path");
 
 var cookieParser = require("cookie-parser");
